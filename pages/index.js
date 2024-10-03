a2916d3b154d026da728c32de2355152416951a9
BobBigHeaded <kieran.hodgson86@gmail.com>
2024-10-03 14:30:01 +0100

    icon and weird bug

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
       className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div class="roundedBox">
+        <div className="roundedBox">
           <Image 
           src={"/image-qr-code.png"} 
           alt="QR code Image"
