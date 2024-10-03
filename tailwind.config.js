d760a9d25619c2ea1031651c4ba594c0738c3c4f
BobBigHeaded <kieran.hodgson86@gmail.com>
2024-10-03 13:22:29 +0100

    main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,23 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.25rem',
+      DEFAULT: '4px',
+      'md': '0.375rem',
+      'lg': '1rem',
+      'full': '9999px',
+      'large': '12px',
+    },
+    colors: {
+      'slate-900': '#1F314F',
+      'slate-500': '#68778D',
     },
   },
   plugins: [],
